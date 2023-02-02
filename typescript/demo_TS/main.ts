b71f91c78e81aacf44b8b2edd5348a70113662ca
GharrowBM <gharrow.games@gmail.com>
2023-02-02 09:25:55 +0100

    Ajout notion TS pour type, tuple et rest operator

--- typescript/demo_TS/main.ts
+++ typescript/demo_TS/main.ts
@@ -2,8 +2,16 @@ console.log("Hello World!")
 
 // Pour créer une fonction en TS, la différence est l'ajout du typage pour les paramètres
 // De la sorte, on sécurise les utilisations futures de notre fonction en vérrouillant des types incorrects
-function addition(a: number, b: number) {
+function addition(a: number, b: number): number {
   return a + b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b
+}
+
+function logSmth(message: string): void {
+  console.log(message);
 }
 
 let result = addition(1, 2)
@@ -47,3 +55,66 @@ let myStatusD: STATUS = 2
 let myPerson: [string, string, number];
 myPerson = ["Albert", "DUPONT", 45]
 
+// Il est possible de créer nos propres types. Pour une utilisation future à plusieurs endroits de notre code 
+// d'une variable pouvant être soit le type A, soit le type B, il est courrant de regrouper cette notion derrière un nouveau type
+// Pour déclarer un nouveau type, la syntaxe est 
+
+// type <nom du type> = <types à regrouper derrière lui>
+
+// Via l'utilisation de l'opérateur '|', on peut spécifier que le type sera soit le type a gauche de l'opérateur, 
+// soit le type à droite de l'opérateur
+
+type monType = string | number
+type monTuple = [string, number]
+
+let myTableA: monType[] = ["10", "a", 25, 20, "true"]
+let myTableB: monTuple = ["true", 10]
+
+// Via la création de type, on peut rendre nos fonctions plus précises, en spécifiant qu'elles peuvent retourner plusieurs 
+// chose en fonction d'une condition par exemple
+function divide(a: number, b: number): monType {
+  try {
+    let result = a / b
+    return result
+  } catch (error) {
+    return "Impossible de diviser par zéro"
+  }
+}
+
+
+// Via la création d'un tuple, on peut retourner plusieurs valeurs dans une fonction de façon optimale pour la mémoire, 
+// et ce sans avoir à stocker un tableau / un objet
+function substractWText (a: number, b: number): [number, string] {
+  let result = a - b
+  return [result, `Le résultat de ${a} - ${b} est ${result}`]
+}
+
+// Le tuple peut être traité tel qu'elle à la réception et au stockage dans une variable
+let resultatSubstract = substractWText(10, 5)
+console.log(resultatSubstract[1]);
+
+// ou directement 'unpacké' pour obtenir X variables directement à partir des éléments du tuple
+let [resultatSub, resultatSubText] = substractWText(10, 5)
+console.log(resultatSubText);
+
+// Via l'utilisation de l'opérateur REST '...' il est possible de créer des fonction pouvant traiter X paramètres, dont leur 
+// nombre sera inconnu. Pour ce faire, il va falloir, au coeur de notre fonction traiter un tableau d'éléments. 
+
+// Durant l'appel, les éléments pourront être envoyés séparéments, mais le temps de l'execution de la fonction, il seront sous forme de tableau
+// Une fois le scope de la fonction terminé, le tableau sera supprimé par le garbage collector, ce qui optimise notre fonction et notre mémoire
+function additionMany(...numbers: number[]) {
+  let sum = 0
+  numbers.forEach(n => {
+    sum += n
+  });
+
+  return sum
+}
+
+console.log(additionMany(1, 2, 3))
+
+// Via l'utilisation de l'opérateur REST, il est aussi possible de séparer ou de fusionner des tableaux très facilement : 
+let monTableauNbA = [1, 2, 3, 4, 5]
+let monTableauNbB = [6, 7, 8, 9, 10]
+let monNouveauTabl = [...monTableauNbA, ...monTableauNbB]
+let [monPremierNb, ...leReste] = monNouveauTabl
