187412d00622e864598d728563331a5181104052
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-02-14 08:36:53 +0100

    suite démos

--- react/demos/cycledevie/src/ComponentLifeCycle.js
+++ react/demos/cycledevie/src/ComponentLifeCycle.js
@@ -3,31 +3,37 @@ import { Component } from "react"
 export class Componentlife extends Component {
     constructor(props){
         super(props)
-        console.log("1- first step -construction")
+        console.log("1- first step - construction")
         this.state = {
             status : true
         }
     }
 
+    changeState(){
+        this.setState((state) => {
+            return {status : !state.status}
+        })
+    }
+
     render() {
-        console.log("2- Second step render")
+        console.log("2- Second step - render")
         return(
             <div>
                 <h1>Rendu</h1>
-                <button onClick={() => this.setState({status : false})}>change status</button>
+                <button onClick={() => this.changeState()}>change status</button>
             </div>
         )
     }
 
     componentDidMount() {
-        console.log("3- third Step end mounting")
+        console.log("3 -- 3th Step - end mouting")
     }
 
     componentDidUpdate(){
-        console.log("4 - end of update")
+        console.log("4- 4th step - end of update")
     }
 
     componentWillUnmount(){
-        console.log("before unmount of componnent")
+        console.log("5- 5th step - before unmount of component")
     }
 }
