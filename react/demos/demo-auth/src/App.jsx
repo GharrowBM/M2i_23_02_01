75fb28fcfb912128620e3ff4d89dbc01f6628146
GharrowBM <gharrow.games@gmail.com>
2023-02-20 15:45:51 +0100

    Ajout des autres requêtes API

--- react/demos/demo-auth/src/App.jsx
+++ react/demos/demo-auth/src/App.jsx
@@ -1,9 +1,15 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ModalComponent from "./components/shared/ModalComponent";
 import { createPortal } from "react-dom";
 import { API_KEY } from "./apiKey";
+import TodoItemForm from "./components/todoitems/TodoItemForm";
+import TodoItemDisplay from "./components/todoitems/TodoItemDisplay";
 
 const App = () => {
+
+  // Pour traiter avec une Base de données de type Realtime Database, il nous faut connaitre l'URL de base de l'endpoint. Cet URL, complété par la suite, sera commun que l'on veuille traiter des contacts, des produits, des jeux, etc...
+  const BASE_DB_URL = "https://m2i-auth-demo-gharr-default-rtdb.europe-west1.firebasedatabase.app/"
+
   // Pour gérer la visibilité de notre modal, il nous faut utiliser une variable de state afin que le rendu de notre appli soit synchronisé avec cette variable
   const [modalVisible, setModalVisible] = useState(false)
 
@@ -16,6 +22,9 @@ const App = () => {
   // Pour travailler avec un formulaire et obtenir les valeurs des champs seulement au moment où l'on va envoyer le formulaire, le plus simple est l'utilisation de useRef()
   const emailRef = useRef()
   const passwordRef = useRef()
+
+  // Pour manipuler des TodoItems, il nous faut une variable de type Array servant à gérer les TodoItems 
+  const [todos, setTodos] = useState([])
   
   // Notre fonction d'envoie de formulaire ayant besoin d'un requêtage API, il nous faut utiliser une version asynchrone (async et await)
   const submitFormHandler = async (event) => {
@@ -78,6 +87,172 @@ const App = () => {
     }
   }
 
+  /*
+    Pour ajouter un TodoItem à notre base de données, il va encore une fois falloir faire une requête API.
+
+    Cette requête API devra prendre en compte le bon endpoint, qui est simplement l'adresse URL de base de notre RealTime Database suivie du nom 
+    du type d'élément que l'on chercher à ajouter. 
+
+    Par exemple, pour ajouter des todoItems, on doit taper l'endpoint : 
+      https://addresse-de-database.com/todoItem.json
+
+    Il va nous falloir passer dans le body de notre requête une version stringifiée de notre objet.
+
+    En retour de la requête POST (pour ajout), nous allons avoir un objet contenant un seul attribut : name. Cet attribut contient l'ID Firebase de notre élément ajouté. Il va nous falloir le manipuler pour synchroniser notre state avec notre database.
+  */
+  const addTodoHandler = async (todoItem) => {
+    console.log(todoItem)
+    try {
+      const token = localStorage.getItem('token')
+      if (token) {
+        const response = await fetch(`${BASE_DB_URL}todoItems.json?auth=${token}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body : JSON.stringify(todoItem)
+        })
+
+        if (!response.ok) {
+          throw new Error("Il y a eu un problème !")
+        }
+
+        const data = await response.json()
+        setTodos([...todos, {id: data.name, ...todoItem}])
+        refreshTodos()
+
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  /*
+    Pour avoir l'accès à nos todoItems, il faut faire une requête de type GET dans le but de récupérer les différents todos. 
+
+    Cette requête n'a pas besoin d'authentification suite au paramétrage de notre projet Firebase. On peut donc envoyer une requête simplifiée sans paramètre de requête 'auth'
+
+    Cependant, le fonctionnement de Firebase se basant sur le principe d'un objet JSON géant, il va nous falloir déplier les clés de notre objet pour ensuite transformer ces clés en objets ajouté à un tableau vierge. Ce tableau servira ensuite à être stocké dans la constante de state 'todos'.
+  */
+
+  const refreshTodos = async () => {
+    try {
+      const response = await fetch(`${BASE_DB_URL}todoItems.json`)
+
+      if (!response.ok) {
+        throw new Error("Il y a eu une erreur lors de la requête GET !")
+      }
+
+      const data = await response.json()
+
+      const tmpTodos = []
+      for (const key in data) {
+        tmpTodos.push({id: key, ...data[key]})
+      }
+      setTodos(tmpTodos)
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    refreshTodos()
+  }, [])
+
+  /*
+    Pour modifier nos todos, il va nous falloir cibler un endpoint spécifique : celui de notre todo à modifier. La modification passera par une requête de type PATCH (pour modification). Cet endpoint sera plus précis que l'endpoint que l'on ciblait précédemment, et ressemblera à ceci : 
+       https://addresse-de-database.com/todoItem/id-du-todoItem.json
+
+    Ayant besoin d'authentification, il va falloir suffixer la requête de '?auth=<valeur du token Firebase de connexion>' tout comme pour l'ajout et la suppresion.
+
+    Le corps de notre requête devra être l'objet modifié, qui finira par remplacer dans le JSON géant de notre base de données l'objet précédemment placé et requêté.
+  */
+
+  const switchTodoStatusHandler = async (todoId) => {
+    const todoItemFound = todos.find(todo => todo.id === todoId)
+    if (todoItemFound) {
+      const token = localStorage.getItem('token')
+      if (token) {
+        try {
+          const newTodoValues = {
+            ...todoItemFound,
+            isDone: !todoItemFound.isDone
+          }
+          const response = await fetch(`${BASE_DB_URL}todoItems/${todoId}.json?auth=${token}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newTodoValues)
+          })
+
+          if (!response.ok) {
+            throw new Error("Erreur durant la requête PATCH !")
+          }
+
+          const data = await response.json()
+
+          console.log(data);
+
+          setTodos([...todos.filter(todo => todo !== todoItemFound), newTodoValues])
+
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+    }
+  }
+
+  /*
+    Dans le cadre d'une suppression, il suffit de taper le même endpoint que lors de la requête PATCH, mais avec cette fois-ci une requête DELETE (pour suppresion). Cette requête ne renvoyant rien de particulier, on peut se passer de notre constante 'data', qui aurait été nulle.
+  */
+
+  const deleteTodoHandler = async (todoId) => {
+    // eslint-disable-next-line no-restricted-globals
+    if(confirm("Etes-vous sûr ?")) {
+      const todoItemFound = todos.find(todo => todo.id === todoId)
+      if (todoItemFound) {
+        try {
+          const token = localStorage.getItem('token')
+          if (token) {
+            const response = await fetch(`${BASE_DB_URL}todoItems/${todoId}.json?auth=${token}`, {
+              method: "DELETE"
+            })
+  
+            if (!response.ok) {
+              throw new Error("Erreur lors de la requête DELETE !")
+            }
+  
+            setTodos([...todos.filter(todo => todo !== todoItemFound)])
+          }
+        } catch (error) {
+          console.error(error.message);
+        }
+
+      }
+    }
+  }
+
+  /*
+    On choisit de trier les todoItems via leur date d'échance (de façon croissante)
+  */
+
+  const sortedTodos = () => {
+    return todos.sort((a, b) => new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime())
+
+    // return todos.sort((a, b) => a.title - b.title)
+  }
+
+  /*
+    Le logOut est simplement le changement du booléen 'connecté' avec écrasement de notre token de connexion dans le stockage local.
+  */
+
+  const logOutHandler = () => {
+    localStorage.removeItem('token')
+    setIsLogged(false)
+  }
+
   return (
     <>
       {/* 
@@ -90,7 +265,7 @@ const App = () => {
       {modalVisible && createPortal(<ModalComponent closeModal={() => setModalVisible(false)}>
         <div className="d-flex justify-content-between align-items center">
         <h3>{isLogging ? 'Sign In' : 'Sign Up'}</h3>
-        <button onClick={() => setModalVisible(false)} className="btn btn-outline-dark rounded-circle"><i className="bi bi-x"></i></button>
+        <button onClick={() =>setModalVisible(false)} className="btn btn-outline-dark rounded-circle"><i className="bi bi-x"></i></button>
         </div>
         <hr />
         <form onSubmit={submitFormHandler}>
@@ -109,12 +284,23 @@ const App = () => {
         </form>
       </ModalComponent>, document.getElementById("modal-root"))}
     <div className="container">
-      <div className="row">
-        <div className="my-3 col-10 offset-2 bg-dark text-light rounded p-3">
-          <h1>App</h1>
-          <hr />
-          <button className="btn btn-primary" onClick={() => setModalVisible(true)}>Show Modal</button>
-          {isLogged && <p>Tu es connecté !</p>}
+      <div className="row g-2 py-3">
+        <div className="col-4">
+          <div className="bg-dark text-light rounded p-3">
+            <TodoItemForm addTodo={addTodoHandler} />
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="bg-dark text-light rounded p-3">
+            <div className="d-flex justify-content-between align-items-center">
+            <h1>App</h1>
+            <button className="btn btn-primary" onClick={() => isLogged ? logOutHandler() : setModalVisible(true)}>{isLogged ? 'Log Out' : 'Show Modal'}</button>
+            </div>
+            <hr />
+            {todos.length === 0 ? 
+              <p>Il n'y a pas de tâches dans la base de données !</p> : 
+              sortedTodos().map(todo => <TodoItemDisplay key={todo.id} todo={todo} switchTodoStatus={switchTodoStatusHandler} deleteTodo={deleteTodoHandler} />)}
+          </div>
         </div>
       </div>
     </div>
