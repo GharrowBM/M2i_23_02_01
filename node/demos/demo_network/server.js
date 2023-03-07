921925d38e638d1e3cb91b64ea84c0964f438cdf
Christophe <you@example.com>
2023-03-07 11:36:07 +0100

    demo network

--- node/demos/demo_network/server.js
+++ node/demos/demo_network/server.js
@@ -9,7 +9,7 @@ const server = net.createServer(function(connection) {
     connection.on('end', function() { // quand la requete client est fini j'affiche 'Client disconnected.'
         console.log('Client disconnected.');
     });
-    connection.write('Hello World!\n');
+    connection.write('Hello World! Coucou envoie client \n');
     connection.pipe(connection);
 // renvoie les données à l'objet de connexion qui est le client
 })
@@ -19,27 +19,3 @@ const server = net.createServer(function(connection) {
 server.listen(8081, function(){  // Port 8081 car 8080 déja utilisé par TomCat
     console.log('Server is listening sur port 8081.');
 });
-
-//console.log('Server Program Ended');
-
-// CTRL + C pour teminer le programme
-
-// Fin partie server
-
-// Debut partie client 
-
-const client = net.connect({port: 8081}, 'localhost', function() {
-    console.log ('Connected to server.')
-})
-
-client.on('data', function(data) {
-    console.log(data.toString());
-    client.end();
-})
-
-client.on('end', function () {
-    console.log('Disconnected from server');
-})
-
-//console.log('Client Program Ended');
-
