053312e335b216c64ac3b1a9071ebdf24d9b8280
Christophe <you@example.com>
2023-03-07 14:13:21 +0100

    demo interaction

--- node/demos/demo_interaction/index.js
+++ node/demos/demo_interaction/index.js
@@ -1,18 +1,5 @@
-import readline from "readline"
-import {stdin, stdout } from "process"
+import { Ihm } from "./classe/ihm.js";
 
-// création d'une fonction pour recupere la saisie dans la console
-const poserUneQuestion = async (question) => {
-    console.log(question)
-    const readlineInterface = readline.createInterface({
-        input : stdin,
-        output : stdout
-    })
-    const result = await (await readlineInterface[Symbol.asyncIterator]().next()).value
 
-    readlineInterface.close()
-    return result
-}
-
-let test = await poserUneQuestion("Merci de saisir du texte :")
-console.log("Vous avez saisi :"+test)
+const ihm = new Ihm()
+ihm.menu()
