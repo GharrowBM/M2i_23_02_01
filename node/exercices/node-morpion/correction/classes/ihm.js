af595b7260dddd8cc2b9aed712c8f94b23242493
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-08 09:27:58 +0100

    suite morpion

--- node/exercices/node-morpion/correction/classes/ihm.js
+++ node/exercices/node-morpion/correction/classes/ihm.js
@@ -1,28 +1,34 @@
+import { Jeu } from "./jeu.js"
+import { poserUneQuestion } from "./tools.js"
 
 
 export class Ihm {
     constructor(){
-      //  this.jeu=new Jeu()
+        this.jeu = new Jeu() 
         this.first = true
         this.fin = false
     }
 
-    getPositions(){
+    async getPositions(){
         const joueur = this.first ? 'X' : 'O'
         console.log("Joueur : "+ joueur)
-        console.log("le joueur choisie sa case")
+       // console.log("le joueur choisie sa case")
+       const x = await poserUneQuestion("Merci de saisir x : ")
+       const y = await poserUneQuestion("Merci de saisir y : ")
+       return { joueur : joueur, x : x, y : y}
     }
 
-    demarrer(){
-        let cpt = 0;
+    async demarrer(){
+       
         while(!this.fin) {
-            const res = this.getPositions()
-            this.first = !this.first
-            // pour tester mes demande de positions au joueurs
-            if(cpt> 5){
-                this.fin = true;
+            const res = await this.getPositions()
+            if(this.jeu.jouer(res.joueur, res.x,res.y)){
+                this.first = !this.first
+                this.jeu.afficher()
+                this.fin = this.jeu.testFin()
             }
-            cpt++;
+           
+            
         }
     }
 
--- node/exercices/node-morpion/correction/classes/jeu.js
+++ node/exercices/node-morpion/correction/classes/jeu.js
@@ -15,4 +15,25 @@ export class Jeu {
             console.log('---------')
         }
     }
+
+    jouer(joueur,x,y) {
+        if(this.board[x-1][y-1] == ' '){
+            this.board[x-1][y-1] = joueur
+            return true
+        }
+        return false
+    }
+
+    testFin() {
+        let test = true
+        for(let i = 0; i < this.board.length; i++){
+            for(let j=0; j < this.board.length; j++){
+                if(this.board[i][j] == ' '){
+                    test = false
+                    break
+                }
+            }
+        }
+        return test
+    }
 }
