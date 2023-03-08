907c18faa455cb951fac6ff95fb724e9b44e08ab
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-08 17:23:09 +0100

    demo csv

---
+++ node/demos/demo-manip-csv/index.js
@@ -0,0 +1,26 @@
+import fs from "fs"
+import LineByline from "n-readlines"
+
+const nom = "toto"
+const prenom = "tata"
+
+fs.appendFileSync("data.csv",`${nom};${prenom}\n`)
+fs.appendFileSync("data.csv",`${nom};${prenom}\n`)
+
+
+const lineReader = new LineByline("data.csv")
+let line
+while(line = lineReader.next()){
+    console.log(line.toString())
+}
+
+const tab = []
+console.log("tab avant ajout data csv", tab)
+const reader = new LineByline("data.csv")
+let myline
+while(myline = reader.next()){
+    const donnees = myline.toString().split(';')
+    const contact = {nom : donnees[0], prenom : donnees[1]}
+    tab.push(contact)
+}
+console.log("tab apres ajout data csv", tab)
