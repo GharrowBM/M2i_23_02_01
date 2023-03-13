881a3559d81bb175b5c42cc9b074dd3d7e117ef2
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-13 14:12:06 +0100

    demo router

--- node/demos/demos_router/router/firstrouter.js
+++ node/demos/demos_router/router/firstrouter.js
@@ -10,10 +10,20 @@ function myCustomMiddleware(req,res,next) {
     next();
 }
 
+function authentification(req,res,next) {
+    const user = req.body.name
+    if(user != "christophe"){
+        return res.status(401).json({message : "non autorisé"})
+    }
+    req.user = user;
+    next();
+}
+
 firstrouter.use(myCustomMiddleware)
+firstrouter.use(authentification)
 
 firstrouter.get('/', (req,res) => {
-    res.json({message : "racine de mon first router"})
+    res.json({message : "racine de mon first router" , utilisateur : req.user})
 })
 
 firstrouter.get('/contact', (req,res) => {
