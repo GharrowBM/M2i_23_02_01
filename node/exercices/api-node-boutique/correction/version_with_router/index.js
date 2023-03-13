cac1bfe249fa0f41ef829bb0c92d327478c197eb
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-13 15:14:14 +0100

    api boutique with router

--- node/exercices/api-node-boutique/correction/version_with_router/index.js
+++ node/exercices/api-node-boutique/correction/version_with_router/index.js
@@ -3,6 +3,8 @@ import { CustomerService } from "./services/customerService.js"
 import { ProductService } from "./services/productService.js"
 import { OrderService } from "./services/orderService.js"
 import productRouter from "./router/productRouter.js"
+import customerRouter from "./router/customerRouter.js"
+import orderRouter from "./router/orderRouter.js"
 
 
 const app = express()
@@ -19,54 +21,11 @@ export const orderService = new OrderService(customerService, productService)
 
 app.use('/produits',productRouter)
 
-
-// creation client
-app.post('/clients', (req, res) => {
-    const { firstName, lastName, phone } = req.body
-    customerService.addCustomer(firstName, lastName, phone)
-    res.json({ message: "client ajouté" })
-})
-
-//recuperation clients
-app.get('/clients', (req, res) => {
-    res.json(customerService.getAllCustomers())
-})
-
-//recuperation client par id
-app.get('/clients/:id', (req, res) => {
-    const customer = customerService.getCustomerById(req.params.id)
-    if (customer != undefined) {
-        res.json(customer)
-    } else {
-        res.json({ message: "aucun client avec cet id" })
-    }
-})
-
-// Recuperation de la liste des commandes
-app.get('/commandes', (req, res) => {
-    res.json(orderService.getAllOrders())
-})
-
-// Recuperation d'un commande par id
-app.get('/commandes/:id', (req, res) => {
-    const order = orderService.getOrderById(req.params.id)
-    if (order != undefined) {
-        res.json(order)
-    } else {
-        res.json({ message: "aucune commande avec cet id" })
-    }
-})
+app.use('/clients',customerRouter)
 
 
-// Création d'une commande
-app.post('/commandes', (req, res) => {
-    const {customerId, products} = req.body
-    if(orderService.addOrder(customerId,products)){
-        res.json({ message: "commande ajouté" })
-    }else {
-        res.json({ message: "erreur" })
-    }
-})
+app.use('/commandes',orderRouter)
+
 
 
 
