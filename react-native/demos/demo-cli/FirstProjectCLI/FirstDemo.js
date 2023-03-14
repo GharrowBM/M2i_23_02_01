c6ddc6f828f240f488d93fa9ae4cfeb96b563d70
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-14 14:14:55 +0100

    debut demo cli

--- react-native/demos/demo-cli/FirstProjectCLI/FirstDemo.js
+++ react-native/demos/demo-cli/FirstProjectCLI/FirstDemo.js
@@ -1,26 +1,56 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 export default function FirstDemo() {
+
+    const tab = [
+        { text : "toto" , id : 1},
+        { text : "tata" , id : 2},
+        { text : "tutu" , id : 3}
+    ]
+
+    function MessageConsole() {
+        console.log("Clique sur le bouton")
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Mon premier composant React Native</Text>
-            <Button title="Mon Bouton" />
+            <Text style={[styles.tailleTexte,styles.monTexte]}>Mon premier composant React Native</Text>
+            <TextInput />
+            <Button title="Mon Bouton" onPress={MessageConsole} />
+            {tab.map((nom,i) => <Text key={i} style={styles.monTexte}>{nom}</Text>)}
+            <FlatList data={tab} renderItem={(itemData) => {
+                return (
+                    <View>
+                        <Text>{itemData.item.text}</Text>
+                    </View>
+                )
+            }} keyExtractor={(item,index) => {
+                return item.id;
+            }}/>
         </View>
     )
 }
 
-//Mauvais pratique
-const container = {
-    backgroundColor: "red",
-    flex: 1,
-}
+//Mauvaise pratique
+// const container = {
+//     backgroundColor: "red",
+//     flex: 1,
+// }
 
 
 //Bonne pratique
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "red",
+        backgroundColor: "blue",
         flex: 1,
+        padding : 20,
+    },
+    monTexte : {
+        margin : 8,
+        color : "white"
+    },
+    tailleTexte : {
+        fontSize : 32
     }
 })
