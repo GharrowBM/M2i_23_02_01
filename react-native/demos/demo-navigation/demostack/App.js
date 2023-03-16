b04d6456ab5d4c05fbd8be080ec51fd1133081fa
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-16 14:22:18 +0100

    debut demo navigation

---
+++ react-native/demos/demo-navigation/demostack/App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+// npm install @react-navigation/native
+// npm install react-native-screens react-native-safe-area-context
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
