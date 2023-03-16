19bc65679e48220b0d48de1905057e28871223d2
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-16 16:42:48 +0100

    debut demo navigation

--- react-native/demos/demo-navigation/demostack/component/PageA.js
+++ react-native/demos/demo-navigation/demostack/component/PageA.js
@@ -6,7 +6,9 @@ export default function PageA({ navigation }) {
     <View>
       <Text>On va mettre un lien vers la page B !!!</Text>
       {/* <Button title='Go to PageB' onPress={() => navigation.navigate("PageB")} /> */}
-      <Button title='Go to PageB' onPress={() => navigation.navigate("PageB", { name : "toto"})} />
+      <Button title='Go to PageB Toto' onPress={() => navigation.navigate("PageB", { firstName : "Toto", lastName : "Dupont" , phone : "0123456789"})} />
+      <Button title='Go to PageB Tata' onPress={() => navigation.navigate("PageB", { firstName : "Tata" , lastName : "Jardin" , phone : "9123456789"})} />
+      <Button title='Go to PageB Titi' onPress={() => navigation.navigate("PageB", { firstName : "Titi", lastName : "Michel" , phone : "6123456789"})} />
     </View>
   )
 }
