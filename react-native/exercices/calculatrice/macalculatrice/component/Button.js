f4e952881ce62bd582647d4176a331affab729d2
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-16 09:13:36 +0100

    debut correction calculatrice

--- react-native/exercices/calculatrice/macalculatrice/component/Button.js
+++ react-native/exercices/calculatrice/macalculatrice/component/Button.js
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Button() {
+//
+export default function Button({title, onPress}) {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <Pressable onPress={onPress} >
+      <View style={styles.isGray}>
+        <Text style={styles.isGrayText}>{title}</Text>
+      </View>
+    </Pressable>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  isGray : {
+    width : 72,
+    height : 72,
+    borderRadius : 16,
+    backgroundColor : "grey",
+    justifyContent : "center",
+    alignItems : "center",
+  },
+  isGrayText : {
+    color : "white",
+    fontSize : 20,
+  }
+})
--- react-native/exercices/calculatrice/macalculatrice/component/Calculatrice.js
+++ react-native/exercices/calculatrice/macalculatrice/component/Calculatrice.js
@@ -4,41 +4,58 @@ import Button from './Button';
 
 export default function Calculatrice() {
 
-    const [firstNumber,setFirstNumber] = useState('0');
-    const [secondNumber,setSecondNumber] = useState('0');
-    const [operation,setOperation] = useState('0');
-    const [result,setResult] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [secondNumber, setSecondNumber] = useState('0');
+  const [operation, setOperation] = useState('0');
+  const [result, setResult] = useState('0');
+
+
+  function numberPress(){
+    console.log("appuie sur un nombre")
+  }
+
+  function operationPress(){
+    console.log("appuie sur un operateur")
+  }
+
+  function resultPress(){
+    console.log("appuie sur un égal")
+  }
+
+  function clear(){
+    console.log("appuie sur clear")
+  }
 
   return (
-    <View>
-        <View>
-      <Text>Mon ecran</Text>
-      </View>
+    <View style={styles.calculatrice}>
       <View>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+        <Text>Mon ecran</Text>
       </View>
-      <View>
-        <Button></Button>
+      <View style={styles.row}>
+        <Button onPress={clear} title ={"AC"} />
         <Button></Button>
         <Button></Button>
         <Button></Button>
       </View>
-      <View>
+      <View style={styles.row}>
         <Button></Button>
         <Button></Button>
         <Button></Button>
         <Button></Button>
       </View>
-      <View>
+      <View style={styles.row}>
         <Button></Button>
         <Button></Button>
         <Button></Button>
         <Button></Button>
       </View>
-      <View>
+      <View style={styles.row}>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+      </View>
+      <View style={styles.row}>
         <Button></Button>
         <Button></Button>
         <Button></Button>
@@ -48,4 +65,14 @@ export default function Calculatrice() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  calculatrice : {
+    width : "100%",
+  },
+  row : {
+    Width : "100%",
+    flexDirection : "row",
+    justifyContent : "space-around",
+    marginVertical : 10,
+  }
+})
