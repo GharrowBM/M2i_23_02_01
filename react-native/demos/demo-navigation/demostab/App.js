8a9db9e955b1b8ff71826b0e4feb5a1b148f6864
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-16 17:00:48 +0100

    debut demo navigation

---
+++ react-native/demos/demo-navigation/demostab/App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './component/Home'
+import Settings from './component/Settings'
+import Contact from './component/Contact'
+
+
+// package necessaire navigation :
+// npm install @react-navigation/native
+// npm install react-native-screens react-native-safe-area-context
+// npm i @react-navigation/bottom-tabs
+
+const Tab = createBottomTabNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator screenOptions={{tabBarIconStyle:{display : 'none'}}}>
+            <Tab.Screen name="Home" component={Home}/>
+            <Tab.Screen name="Setting" component={Settings}/>
+            <Tab.Screen name="Contact" component={Contact}/>
+        </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
