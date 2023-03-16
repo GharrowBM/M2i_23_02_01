3b841d251d487e696c32dcbfa1ecfdb7f9727751
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-16 14:38:11 +0100

    debut demo navigation

--- react-native/demos/demo-navigation/demostack/App.js
+++ react-native/demos/demo-navigation/demostack/App.js
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import PageA from './component/PageA'
+import PageB from './component/PageB'
 
+
+// package necessaire navigation :
 // npm install @react-navigation/native
 // npm install react-native-screens react-native-safe-area-context
+// npm i @react-navigation/native-stack
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="PageA" component={PageA}/>
+                <Stack.Screen name="PageB" component={PageB}/>
+            </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
