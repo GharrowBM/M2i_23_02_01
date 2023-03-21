44766d26613ddd33513c7b6c268a98fdc2813fbd
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-21 17:09:07 +0100

    demo axios firebase

--- react-native/demos/demo-request/demorequest/App.js
+++ react-native/demos/demo-request/demorequest/App.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { requestPost } from './util/http'
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { requestGet, requestPost, requestPostWithResponse } from './util/http'
 
 export default function App() {
 
-    requestPost({name : "toto" , mail : "toto@toto.com"})
+   // requestPost({name : "tonton" , mail : "tonton@tonton.com"})
+
+   const [load,setLoad] = useState(true)
+   const [users,setUsers] = useState([]);
+
+   useEffect(() => {
+    async function getMyData(){
+      setLoad(true)
+      const data = await requestGet();
+      setLoad(false)
+      setUsers(data)
+    }
+    getMyData();
+   },[])
+
+   async function PostSomething(){
+    const id = await requestPostWithResponse({ name : "titi" , mail : "titi@titi.com"})
+    setUsers((state) => {
+      return [...state,{id : id, name : "titi" , mail : "titi@titi.com"}]
+    })
+   }
+
+   if(load){
+    return(
+      <View>
+        <ActivityIndicator size="large" color="#00ff00"></ActivityIndicator>
+      </View>
+    )
+   }
 
   return (
     <View>
-      <Text>App</Text>
+      <Text>Mes contacts :</Text>
+      {users.map((u) => <Text key={u.id}>nom : {u.name} mail : {u.mail}</Text>)}
+      <Text>Ajout d'un client :</Text>
+      <Button title='Ajout de titi' onPress={PostSomething}/> 
     </View>
   )
 }
