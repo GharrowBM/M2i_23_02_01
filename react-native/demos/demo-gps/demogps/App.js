218d9dbf59146538a9d5d416c9fd0ccb72288de5
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-22 15:59:30 +0100

    demo gps

--- react-native/demos/demo-gps/demogps/App.js
+++ react-native/demos/demo-gps/demogps/App.js
@@ -1,10 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Geolocation from '@react-native-community/geolocation'
 
+
+// npm i @react-native-community/geolocation
 export default function App() {
+    const [latitude, setLatitude] = useState(null)
+    const [longitude, setLongitude] = useState(null)
+
+    useEffect(() => {
+
+        Geolocation.getCurrentPosition(
+            position => {
+                setLatitude(position.coords.latitude)
+                setLongitude(position.coords.longitude)
+            },
+            error => console.log(error),
+            {enableHighAccuracy :true, timeout : 20000, maximumAge : 1000 }
+        );
+
+    },[])
+
   return (
     <View>
-      <Text>App</Text>
+      <Text>Latitude : {latitude}</Text>
+      <Text>Longitude : {longitude}</Text>
     </View>
   )
 }
