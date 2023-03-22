2af7f230e458aa9bc8d240440daa650a8f3b18ad
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-22 13:45:28 +0100

    demo async storage

--- react-native/demos/demos_async/demoasync/App.js
+++ react-native/demos/demos_async/demoasync/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-    const [value,setValue] = useState({ firstName : '', lastName : ''});
+    const [value, setValue] = useState({ firstName: '', lastName: '' });
 
     // Demo avec stockage String
 
@@ -38,25 +38,42 @@ export default function App() {
     // Demo avec stockage Objet
 
     const addData = async () => {
-
+        try {
+            const maValue = JSON.stringify({ firstName: 'tata', lastName: 'titi' })
+            await AsyncStorage.setItem('key', maValue)
+        } catch (error) {
+            console.log(error)
+        }
     }
     const getData = async () => {
-        
+        try {
+            const maValue = await AsyncStorage.getItem('key')
+            if (maValue !== null) {
+                setValue(JSON.parse(maValue))
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
     const removeData = async () => {
-        
+        try {
+            await AsyncStorage.removeItem('key')
+            setValue({ firstName: '', lastName: '' })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-  return (
-    <View>
-      {/* <Text style={{fontSize : 32}}>Valeur stockée : {value}</Text> */}
-      <Text style={{fontSize : 32}}>Nom : {value.firstName}</Text>
-      <Text style={{fontSize : 32}}>Nom : {value.lastName}</Text>
-      <Button title='Add Data' onPress={addData}/>
-      <Button title='Get Data' onPress={getData}/>
-      <Button title='Delete Data' onPress={removeData}/>
-    </View>
-  )
+    return (
+        <View>
+            {/* <Text style={{fontSize : 32}}>Valeur stockée : {value}</Text> */}
+            <Text style={{ fontSize: 32 }}>Prenom : {value.firstName}</Text>
+            <Text style={{ fontSize: 32 }}>Nom : {value.lastName}</Text>
+            <Button title='Add Data' onPress={addData} />
+            <Button title='Get Data' onPress={getData} />
+            <Button title='Delete Data' onPress={removeData} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({})
