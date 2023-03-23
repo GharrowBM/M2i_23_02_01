6b40ee3c380e6b4e70ae0628494e27af56876b85
Delory Christophe <christophe.delory@labom2iformation.fr>
2023-03-23 09:21:21 +0100

    exercice gps api

---
+++ react-native/exercices/localisation/App.js
@@ -0,0 +1,55 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Geolocation from '@react-native-community/geolocation'
+
+
+// npm i @react-native-community/geolocation
+// ajout de la ligne : <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+// dans android/app/src/main/AndroidManifest.xml
+
+export default function App() {
+    const [latitude, setLatitude] = useState(null)
+    const [longitude, setLongitude] = useState(null)
+    const [localisationFetch,setLocalisationFetch] = useState({ ville : '' , pays : ''})
+    const [localisationAxios,setLocalisationAxios] = useState({ ville : '' , pays : ''})
+    const [getPosition,setGetPosition] = useState(true)
+
+    useEffect(() => {
+        Geolocation.requestAuthorization()
+        Geolocation.getCurrentPosition(
+            position => {
+                setLatitude(position.coords.latitude)
+                setLongitude(position.coords.longitude)
+            },
+            error => console.log(error),
+            {enableHighAccuracy :true, timeout : 20000, maximumAge : 1000 }
+        );
+
+    },[getPosition])
+
+    function getApiWithFetch(){
+        console.log("demande API avec Fetch")
+    }
+
+    function getApiWithAxios(){
+      console.log("demande API avec Axios")
+    }
+
+    function reload(){
+      setGetPosition(state => !state)
+    }
+
+  return (
+    <View>
+      <Button title='get coord' onPress={reload}/>
+      <Text>Latitude : {latitude}</Text>
+      <Text>Longitude : {longitude}</Text>
+      <Button title='Get localisation with Fetch' onPress={getApiWithFetch}/>
+      <Text>Localisation : {localisationFetch.pays} {localisationFetch.ville}</Text>
+      <Button title='Get localisation with Axios' onPress={getApiWithAxios}/>
+      <Text>Localisation : {localisationAxios.pays} {localisationAxios.ville}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
